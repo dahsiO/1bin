57d0c9ece470f56c805ca5d4290927d10be94e94
Ousd173 <dahsioussama@gmail.com>
2025-10-16 18:57:51 +0200

    -m 'h1'

---
+++ Q1/algo/semaine4/exo/AlgoWhile/src/CalculMoyenne.java
@@ -0,0 +1,39 @@
+public class CalculMoyenne {
+
+    public static java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        // la chaine de caracteres se met entre""
+        // Le caractere se met entre ''
+        char reponse = 'Y';
+
+        // Pour lire un caractere :
+        // (next() --> lecture d'une chaine de caracteres)
+        //(charAt(0) --> 1er caractere de la chaine de caracteres
+        reponse = scanner.next().charAt(0);
+
+
+        // A COMPLETER
+
+    }
+
+}
+
+
+
+
+
+
+
+
+char reponse = 'Y';
+int sommeDesCote = 0;
+
+double cote = scanner.nextDouble();
+        System.out.println("il y a une autres cote");
+
+         while (reponse == 'Y'){
+
+
+        }
---
+++ Q1/algo/semaine4/exo/AlgoWhile/src/MultiplicationAuFinish.java
@@ -0,0 +1,23 @@
+
+public class MultiplicationAuFinish {
+	
+	public static java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		System.out.println("Entrainement aux multiplications. Voici une multiplication :");
+		System.out.println("Tant que tu n'as pas trouve la bonne reponse, tu dois la recommencer! ");
+
+		int premierNombre = unEntierAuHasardEntre(0, 10);
+		int secondNombre = unEntierAuHasardEntre(0, 10);
+		System.out.println("Calculez : " + premierNombre + " x " + secondNombre + " = ");
+		// A COMPLETER
+
+
+	}
+
+	// A NE PAS MODIFIER
+	public static int unEntierAuHasardEntre (int valeurMinimale, int valeurMaximale){
+		return (int) (Math.random() * (valeurMaximale - valeurMinimale + 1)) + valeurMinimale;
+	}
+}
