6adc07906297ea3b78a1313030bf647bc79374f0
Ousd173 <dahsioussama@gmail.com>
2026-02-18 16:34:02 +0100

    Syllabus_TS

--- Q2/TS/semaine1/exo/server/server/dist/controllers/doctors.controller.js
+++ Q2/TS/semaine1/exo/server/server/dist/controllers/doctors.controller.js
@@ -5,6 +5,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.doctorsController = void 0;
 const express_1 = require("express");
+const guards_1 = require("../utils/guards");
 exports.doctorsController = (0, express_1.Router)();
 // This is a static mock array of doctors
 const doctors = [
@@ -18,4 +19,25 @@ const doctors = [
 exports.doctorsController.get("/", (req, res) => {
     console.log("[GET] /doctors/");
     res.json(doctors).status(200);
+});
+// GET /doctors/1  →  retourne le docteur avec id=1
+exports.doctorsController.get("/:id", (req, res) => {
+    console.log("[GET] /doctors/:id");
+    // 1️⃣ Transformer le paramètre en nombre
+    const id = Number(req.params.id);
+    // 2️⃣ Vérifier que c’est bien un nombre valide
+    if (!(0, guards_1.isNumber)(id)) {
+        res.status(400).send("ID must be a number");
+        return;
+    }
+    // 3️⃣ Parcourir la liste avec une boucle for
+    for (let i = 0; i < doctors.length; i++) {
+        if (doctors[i].id === id) {
+            // 5️⃣ Docteur trouvé
+            res.status(200).json(doctors[i]);
+            return;
+        }
+    }
+    // 4️⃣ Docteur non trouvé
+    res.status(404).send("Doctor not found");
 });
--- Q2/TS/semaine1/exo/server/server/src/controllers/doctors.controller.ts
+++ Q2/TS/semaine1/exo/server/server/src/controllers/doctors.controller.ts
@@ -3,6 +3,7 @@
  */
 
 import { Request, Response, Router } from "express";
+import { isNumber } from "../utils/guards"; 
 import { Doctor } from "../models/doctor.model";
 
 export const doctorsController = Router();
@@ -21,3 +22,32 @@ doctorsController.get("/", (req: Request, res: Response) => {
   console.log("[GET] /doctors/");
   res.json(doctors).status(200);
 });
+
+
+// GET /doctors/1  →  retourne le docteur avec id=1
+doctorsController.get("/:id", (req: Request, res: Response) => {
+  console.log("[GET] /doctors/:id");
+
+    // 1️⃣ Transformer le paramètre en nombre
+  const id = Number(req.params.id);
+
+  // 2️⃣ Vérifier que c’est bien un nombre valide
+  if (!isNumber(id)) {
+    res.status(400).send("ID must be a number");
+    return;
+  }
+
+  // 3️⃣ Parcourir la liste avec une boucle for
+  for (let i = 0; i < doctors.length; i++) {
+    if (doctors[i].id === id) {
+
+      // 5️⃣ Docteur trouvé
+      res.status(200).json(doctors[i]);
+      return;
+    }
+  }
+
+  // 4️⃣ Docteur non trouvé
+  res.status(404).send("Doctor not found");
+});
+ 
